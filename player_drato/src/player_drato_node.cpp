7aa6f63f24001cae8570f3bab06e9a811f5703fb
Daniela Rato <danielarato@ua.pt>
2019-02-21 15:11:17 +0000

    Ros launch OK

--- player_drato/src/player_drato_node.cpp
+++ player_drato/src/player_drato_node.cpp
@@ -1,14 +1,63 @@
+/**
+ * @file player_drato_node.cpp
+ * @author Daniela Rato (danielarato@ua.pt)
+ * @brief 
+ * @version 0.1
+ * @date 2019-02-21
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
 #include <iostream>
-
 #include <ros/ros.h>
-
 #include <vector>
 
 using namespace std;
+using namespace boost;
+using namespace ros;
 // using namespace cv;
 
 namespace drato_ns
 {
+
+class Team
+{
+  public:
+    string team_name;
+    vector<string>
+        player_names;
+    ros::NodeHandle n;
+
+    Team(string team_name_in)
+    {
+        team_name = team_name_in;
+        n.getParam("/team_" + team_name, player_names);
+    }
+
+    void printInfo()
+    {
+        cout << "Team " << team_name << " has players: " << endl;
+        for (size_t i = 0; i < player_names.size(); cout << player_names[i++] << endl)
+            ;
+    }
+
+    bool playerBelongsToTeam(string player_name)
+    {
+        for (size_t i = 0; i < player_names.size(); i++)
+        {
+            if (player_name == player_names[i])
+            {
+                // cout << "Belongs to team" << endl;
+                return true;
+            }
+        }
+        // cout << "Doesn't belong" << endl;
+        return false;
+    }
+
+  private:
+};
 
 class Player
 {
@@ -57,46 +106,54 @@ class Player
 
 class MyPlayer : public Player
 {
+    boost::shared_ptr<Team> team_red;
+    boost::shared_ptr<Team> team_green;
+    boost::shared_ptr<Team> team_blue;
+    boost::shared_ptr<Team> team_hunters;
+    boost::shared_ptr<Team> team_mine;
+    boost::shared_ptr<Team> team_preys;
+
   public:
     MyPlayer(string player_name_in, string team_name_in) : Player(player_name_in)
     {
-        setTeamName(team_name_in);
-    }
+        team_red = (boost::shared_ptr<Team>)new Team("red");
+        team_green = (boost::shared_ptr<Team>)new Team("green");
+        team_blue = (boost::shared_ptr<Team>)new Team("blue");
 
-  private:
-};
+team_red->printInfo();
+team_green->printInfo();
+team_blue->printInfo();
+        cout << player_name << endl;
 
-class Team
-{
-  public:
-    string team_name;
-    vector<string>
-        player_names;
+        if (team_red->playerBelongsToTeam(player_name))
+        {
+            team_mine = team_red;
+            team_preys = team_green;
+            team_hunters = team_blue;
+        }
+        else if (team_green->playerBelongsToTeam(player_name))
+        {
+            team_mine = team_green;
+            team_preys = team_blue;
+            team_hunters = team_red;
+        }
+        else if (team_blue->playerBelongsToTeam(player_name))
+        {
+            team_mine = team_blue;
+            team_preys = team_red;
+            team_hunters = team_green;
+        }
+        else {cout <<"Something wrong in team parametrization!!" << endl;}
 
-    Team(string team_name_in)
-    {
-        team_name = team_name_in;
+
+        setTeamName(team_mine->team_name);
+        printInfo();
     }
 
     void printInfo()
     {
-        cout << "Team " << team_name << " has players: " << endl;
-        for (size_t i = 0; i < player_names.size(); cout << player_names[i++] << endl)
-            ;
-    }
-
-    bool playerBelongsToTeam(string player_name)
-    {
-        for (size_t i = 0; i < player_names.size(); i++)
-        {
-            if (player_name == player_names[i])
-            {
-                // cout << "Belongs to team" << endl;
-                return true;
-            }
-        }
-        // cout << "Doesn't belong" << endl;
-        return false;
+        ROS_INFO_STREAM("My name is " << player_name << " and my team is " << team_mine->team_name);
+        ROS_INFO_STREAM("I am hunting " << team_preys->team_name << " and fleeing from " << team_hunters->team_name);
     }
 
   private:
@@ -106,29 +163,37 @@ class Team
 
 main(int argc, char *argv[])
 {
-    drato_ns::MyPlayer player("drato", "blue");
-    // Player player("drato");
+
+    ros::init(argc, argv, "drato");
+    drato_ns::MyPlayer player("moliveira", "blue");
+    ros::NodeHandle n;
+    // string player_name = "drato";
     // player.setTeamName("blue");
     // player.setTeamName(2);
 
-    cout << "Hello world from " << player.player_name << " of team " << player.getTeamName() << endl;
+    // cout << "Hello world from " << player.player_name << " of team " << player.getTeamName() << endl;
 
-    drato_ns::Team team_blue("blue");
-    team_blue.player_names.push_back("drato");
-    team_blue.player_names.push_back("blourenco");
+    // drato_ns::Team team_blue("blue");
+    // team_blue.player_names.push_back("drato");
+    // team_blue.player_names.push_back("blourenco");
 
-    team_blue.printInfo();
-    cout << "drato belongs to team? " << team_blue.playerBelongsToTeam("drato") << endl;
+    while (ros::ok())
+    {
+        // cout << "Created an instance of class player with public name " << player_name << " of team " << player.getTeamName() << endl;
+        // team_blue.printInfo();
+        // cout << "drato belongs to team? " << team_blue.playerBelongsToTeam("drato") << endl;
+        ros::Duration(1).sleep();
+    }
 
-        //cenas que nao interessam
-        // ros::init(argc, argv, "player_drato_node");
+    //cenas que nao interessam
+    // ros::init(argc, argv, "player_drato_node");
 
-        // ros::NodeHandle n;
+    // ros::NodeHandle n;
 
-        // for (int i = 0; i < 10; i++)
-        // {
-        //     cout << i << endl;
-        // }
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout << i << endl;
+    // }
 
-        return 0;
+    return 0;
 }
